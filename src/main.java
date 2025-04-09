53cc2701f5f12c5a44f453f8ab8b53e5fe30c4de
MaryLCrea <maryleenen@yahoo.com>
2025-04-09 23:28:53 +0200

    kleine aanpassingen in de tekst

--- src/main.java
+++ src/main.java
@@ -6,33 +6,33 @@ public class main {
         postiveOrZeroOrNegativ(0);
         bartender("Lisa");
         int result = sum(3, 7);
-        System.out.println("De som is: " + result);
+        System.out.println("The sum is: " + result);
     }
 
     public static void hello() {
-        System.out.println("Hallo!");
+        System.out.println("Hello World!");
     }
 
     public static void positiveOrNegative(int number) {
         if (number >= 0) {
-            System.out.println(number + " is positief.");
+            System.out.println(number + " this number is positive.");
         } else {
-            System.out.println(number + " is negatief.");
+            System.out.println(number + " this number is negative.");
         }
     }
 
     public static void postiveOrZeroOrNegativ(int number) {
         if (number > 0) {
-            System.out.println(number + " is positief.");
+            System.out.println(number + " this number is positive.");
         } else if (number == 0) {
-            System.out.println("Het getal is nul.");
+            System.out.println("This number is zero!");
         } else {
-            System.out.println(number + " is negatief.");
+            System.out.println(number + " This number is negative.");
         }
     }
 
     public static void bartender(String name) {
-        System.out.println("Welkom in de bar, " + name + "!");
+        System.out.println("Welkom in de bar, ik weet dat je graag bacardi drinkt " + name + "!");
     }
 
     public static int sum(int input1, int input2) {
